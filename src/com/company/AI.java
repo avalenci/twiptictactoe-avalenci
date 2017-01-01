c06d69e3b406d982f252a06521b133c928333e67
avalenci <ansmeew@gmail.com>
2016-12-31 19:45:07 -0500

    Seventh commit.

--- src/com/company/AI.java
+++ src/com/company/AI.java
@@ -1,61 +1,19 @@
 package com.company;
 
 public class AI {
-    static String firstPlayer() {
+    public static String firstPlayer() {
         int rand = (int) (Math.random() * 2);
         if (rand == 0)
             return "O";
         else
             return "X";
     }
-    static String firstMove() {
-        int rand = (int) (Math.random() * 5);
-        if (rand == 0)
-            return "00";
-        else if (rand == 1)
-            return "02";
-        else if (rand == 2)
-            return "11";
-        else if (rand == 3)
-            return "20";
-        else
-            return "22";
-    }
-    static String secondMove(int x, int y) {
-        if (x == 1 && y == 1) {
-            int z = (int) (Math.random() * 4);
-            if (z == 0)
-                return "00";
-            else if (z == 1)
-                return "02";
-            else if (z == 2)
-                return "20";
-            else
-                return "22";
+    public static String deleteNumbers(String a) {
+        for (int x = 1; x < 10; x++) {
+            String y = Integer.toString(x);
+            char z = y.charAt(0);
+            a = a.replace(z, ' ');
         }
-        else
-            return "11";
-    }
-    static String thirdMove(char a[][]) {
-        String t;
-        while (true) {
-            int x = (int) (Math.random() * 3);
-            int y = (int) (Math.random() * 3);
-            String z = "" + a[x][y];
-            if (!(z.equals("X") || z.equals("O"))) {
-                t = Integer.toString(x) + Integer.toString(y);
-                break;
-            }
-        }
-        return t;
-    }
-    static String nextMove(char a[][], String player) {
-        String enemy;
-        if (player.equalsIgnoreCase("X"))
-            enemy = "O";
-        else
-            enemy = "X";
-
-        return "00";
+        return a;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,90 +5,77 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String player, Computer, firstPlayer;
         String grid = " 1 | 2 | 3 \n---+---+---\n 4 | 5 | 6 \n---+---+---\n 7 | 8 | 9 ";
         char numb[][] = {{'1', '2', '3'},
                          {'4', '5', '6'},
                          {'7', '8', '9'}};
-        int turns = 0;
         int xCoord = 0, yCoord = 0;
+        int turns = 0;
         while (true) {
-            System.out.println("Type \"Next\" to keep the game going.");
-            String player = AI.firstPlayer();
+            System.out.println("Would you like to be X or O?");
             while (true) {
-                if (player.equalsIgnoreCase("X"))
-                    player = "O";
-                else
+                String a = input.next();
+                if (a.equalsIgnoreCase("X")) {
                     player = "X";
-                if (turns == 0) {
-                    String c = AI.firstMove();
-                    String x = (c.substring(0, 1));
-                    String y = (c.substring(1));
-                    xCoord = Integer.parseInt(x);
-                    yCoord = Integer.parseInt(y);
+                    Computer = "O";
                 }
-                else if (turns == 1) {
-                    String c = AI.secondMove(xCoord, yCoord);
-                    String x = (c.substring(0, 1));
-                    String y = (c.substring(1));
-                    xCoord = Integer.parseInt(x);
-                    yCoord = Integer.parseInt(y);
+                else if (a.equalsIgnoreCase("O")) {
+                    player = "O";
+                    Computer = "X";
                 }
-                else if (turns == 2) {
-                    String c = AI.thirdMove(numb);
-                    String x = (c.substring(0, 1));
-                    String y = (c.substring(1));
-                    xCoord = Integer.parseInt(x);
-                    yCoord = Integer.parseInt(y);
-                }
-                else if (turns > 2) {
-                    String c = AI.nextMove(numb, player);
-                    String x = (c.substring(0, 1));
-                    String y = (c.substring(1));
-                    xCoord = Integer.parseInt(x);
-                    yCoord = Integer.parseInt(y);
-                }
-                if (!((input.nextLine()).equalsIgnoreCase("Next")))
+                else {
+                    System.out.println("Try Again.");
                     continue;
-                while (player.equalsIgnoreCase("X")) {
-                    if (turns == 0) {
-                        grid = grid.replace(numb[xCoord][yCoord], 'X');
-                        numb[xCoord][yCoord] = 'X';
-                    }
-                    else if (turns == 1) {
-                        grid = grid.replace(numb[xCoord][yCoord], 'X');
-                        numb[xCoord][yCoord] = 'X';
-                    }
-                    else if (turns == 2) {
-                        grid = grid.replace(numb[xCoord][yCoord], 'X');
-                        numb[xCoord][yCoord] = 'X';
-                    }
-                    else {
-                        grid = grid.replace(numb[xCoord][yCoord], 'X');
-                        numb[xCoord][yCoord] = 'X';
-                    }
-                    break;
                 }
-                while (player.equalsIgnoreCase("O")) {
-                    if (turns == 0) {
-                        grid = grid.replace(numb[xCoord][yCoord], 'O');
-                        numb[xCoord][yCoord] = 'O';
+                System.out.println("Who will go first?\nA. You\nB. Computer\nC. Random");
+                while (true) {
+                    a = input.next();
+                    if (a.equalsIgnoreCase("A"))
+                        firstPlayer = player;
+                    else if (a.equalsIgnoreCase("B"))
+                        firstPlayer = Computer;
+                    else if (a.equalsIgnoreCase("C")) {
+                        firstPlayer = AI.firstPlayer();
+                        System.out.println("The first player is " + firstPlayer + ".");
+                    } else {
+                        System.out.println("Try Again.");
+                        continue;
                     }
-                    else if (turns == 1) {
-                        grid = grid.replace(numb[xCoord][yCoord], 'O');
-                        numb[xCoord][yCoord] = 'O';
+                    while (true) {
+                        while (firstPlayer.equals(player)) {
+                            System.out.println(AI.deleteNumbers(grid));
+                            System.out.println("Type the two numbers that correspond with the coordinates of your move.");
+                            while (true) {
+                                a = input.nextLine();
+                                a = a.replaceAll(" ", "");
+                                String a0 = a.substring(0);
+                                String a1 = a.substring(0);
+                                if ((a0 != "0" && a0 != "1" && a0 != "2") || (a1 != "0" && a1 != "1" && a1 != "2")) {
+                                    System.out.println("Please use valid coordinates only.\nTry Again.");
+                                    continue;
+                                }
+                                xCoord = Integer.parseInt(a0);
+                                yCoord = Integer.parseInt(a1);
+                                if (numb[xCoord][yCoord] == 'X' || numb[xCoord][yCoord] == 'O') {
+                                    System.out.println("That spot is already taken!\nTry Again.");
+                                    continue;
+                                }
+                                else {
+                                    char replace = numb[xCoord][yCoord];
+                                    numb[xCoord][yCoord] = player.charAt(0);
+                                    grid = grid.replace(replace, player.charAt(0));
+                                    break;
+                                }
+                            }
+                            turns++;
+                            firstPlayer = Computer;
+                        }
+                        while (firstPlayer.equals(Computer)) {
+
+                        }
                     }
-                    else if (turns == 2) {
-                        grid = grid.replace(numb[xCoord][yCoord], 'O');
-                        numb[xCoord][yCoord] = 'O';
-                    }
-                    else {
-                        grid = grid.replace(numb[xCoord][yCoord], 'O');
-                        numb[xCoord][yCoord] = 'O';
-                    }
-                    break;
                 }
-                System.out.println(Clear.deleteNumbers(grid));
-                turns++;
             }
         }
     }
